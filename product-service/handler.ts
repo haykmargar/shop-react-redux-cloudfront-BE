ad9018d9e7387faca26451a20af33b6b6cc01bb4
Hayk_Margaryan1 <Hayk_Margaryan1@epam.com>
2024-05-04 17:29:23 +0400

    Task6-SQS-SNS

--- product-service/handler.ts
+++ product-service/handler.ts
@@ -3,3 +3,4 @@ import * as handlers from './src';
 export const getProductById = handlers.getProductByIdHandler();
 export const getProductsList = handlers.getProductsListHandler();
 export const createProduct = handlers.createProductHandler();
+export const catalogBatchProcess = handlers.catalogBatchProcessHandler();
